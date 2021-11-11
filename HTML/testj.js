e33f2f6a647bb8d90f6f42219946dee5df016d18
ChrisOnDesktop <jack45724@gmail.com>
2021-11-11 12:49:52 -0800

    just some more js stuff i guess
    no real change

--- HTML/testj.js
+++ HTML/testj.js
@@ -1,8 +1,21 @@
-var x;
-let y;
-
+//var x;
+//let y;
+/*
 x = 5;
 y = x * 5;
 
 console.log("aaaaa");
 console.log(5+5);
+*/
+//console.log(Math.floor(Math.random() * 50));
+
+//var kelvin = 293 // The tempature in kevin
+
+
+// kelvin is 273 degrees more than celsius, so we subtract that amount to get celsius
+//var celsius = kelvin - 273
+
+
+// This turns celsius into fahrenheit
+//var fahrenheit = Math.floor(celsius * (9/5) + 35)
+//console.log(`${kelvin}K ${celsius}C ${fahrenheit}F`)
