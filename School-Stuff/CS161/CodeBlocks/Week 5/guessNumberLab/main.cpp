3ba50c7dda24609bccb8e8f6eb0680c995051c7d
ChrisOnDesktop <jack45724@gmail.com>
2022-02-02 20:35:09 -0800

    a

--- School-Stuff/CS161/CodeBlocks/Week 5/guessNumberLab/main.cpp
+++ School-Stuff/CS161/CodeBlocks/Week 5/guessNumberLab/main.cpp
@@ -1,10 +1,16 @@
+/*
+Chris Howard
+Guess a number lab
+CS161
+*/
+
 #include <iostream>
 #include <time.h>
 
 using namespace std;
 
-int main()
 
+int main()
 {
     int target, guess;
     int tries = 0;
@@ -19,9 +25,10 @@ int main()
 
         target = (rand()%100)+1;
         tries = 0;
+        cout << endl;
 
         do {
-            cout << "Target is: " << target << endl;
+            //cout << "Target is: " << target << endl;
 
             cout << "Enter a guess (1-100): ";
             cin >> guess;
@@ -32,7 +39,7 @@ int main()
                 cin.clear();
                 cin.ignore(255,'\n');
                 cout <<"invalid choice\n";
-                cout << 5-invalidLoop << " attempts remaining";
+                cout << 5-invalidLoop << " attempts remaining\n";
                 cout << "Enter a guess (1-100): ";
                 cin >> guess;
                 invalidLoop++;
@@ -52,6 +59,29 @@ int main()
                 cout << endl;
                 cout << guess <<" Hit target!" << endl;
                 cout << "Guess was: " << guess << endl;
+
+                if(tries == 0){
+                    cout << "It took you " << tries + 1<<" try!" << endl;
+                    cout << "you good, or do you cheat?" << endl;
+                }
+                else if(tries < 2){
+                    cout << "It took you " << tries + 1<<" tries!" << endl;
+                    cout << "Amazing!" << endl;
+                }
+                else if(tries < 5){
+                    cout << "It took you " << tries + 1<<" tries!" << endl;
+                    cout << "Awesome!" << endl;
+                }
+                else if(tries < 10){
+                cout << "It took you " << tries + 1<<" tries!" << endl;
+                cout << "Good job!" << endl;
+                }
+                else{
+                    cout << "It took you " << tries + 1<<" tries!" << endl;
+                }
+                
+
+                
             }
             else if(guess < target)
             {
@@ -63,12 +93,28 @@ int main()
                 cout << "This is an error\nYou should not see this\n";
             }
             
+
+
+            // I'm too lazy to make this more efficient 
+            if(target != guess){
+                if(tries == 0){
+                    cout << "you've made " << tries+1 << " guess\n";
+                }
+                else{
+                cout << "you've made " << tries+1<< " guesses\n";
+            }
             tries++;
+
+            }
+
+            
 
         }while(guess != target);
 
-        cout << "Would you like to play again?\n[y]es [n]o: ";
+        cout << "\nWould you like to play again?\n[y]es [n]o: ";
         cin >> gameOn;
+        cin.clear();
+        cin.ignore(255,'\n');
 
 
     }
