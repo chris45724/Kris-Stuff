150f54d20f8ceab7cad2efbd2c7dd91e32b1a5c6
chris45724 <jack45724@gmail.com>
2022-04-17 23:02:16 -0700

    more week 3 stuff

--- School-Stuff/Misc School/test.cpp
+++ School-Stuff/Misc School/test.cpp
@@ -2,6 +2,7 @@
 #include <time.h>
 #include <random>
 #include <iomanip>
+#include <cctype>
 
 
 using namespace std;
@@ -9,50 +10,11 @@ using namespace std;
 // dynamicly change size of array
 
 //template <typename T>
-void swapArray(int *arr[],int a, int b){
-    int c = a;
-    arr[a] = arr[b];
-    arr[b] = arr[c];
-    
-}
-template <typename T>
-void arraySort(T* array[], int size){
-    bool sorted = false;
-    int minimum = 0;
-    int place = 0;
-    while(place < size){
-        minimum = place;
-        for(int x = place; x < size; x++){
-            if (array[minimum] > array[x]){
-                minimum = x;
-                sorted = false;
-            }
-        }
-        cout << "swapping " << array[minimum] << " with "  << array[place] << endl;
-        swapArray(array,minimum,place);
-        place++;
-        //cout << "Sorted : " << sorted << endl;
-
-    }
-
-
-}
 
 
 
 int main(){
-    srand(time(nullptr));
-    int size = 20;
-    int* array[size];
-    for(int x = 0; x < size; x++){
-        *array[x] = (rand()%40+1);
-        cout << array[x] << " ";
+    if (!isdigit('q')){
+        cout << "yes";
     }
-    cout << '\n';
-    arraySort(array,size);
-    for(int x = 0; x < size; x++){
-        cout << array[x] << " ";
-    }
-    cout << '\n';
-
 }
